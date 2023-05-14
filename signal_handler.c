9de44dcf891768c48a235b8e7b03f83aec58f3d1
Anir Zaher <azaher@e2r11p3.1337.ma>
2023-05-14 16:27:48 +0100

    pulling

--- signal_handler.c
+++ signal_handler.c
@@ -6,24 +6,24 @@
 /*   By: azaher <azaher@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 16:23:25 by azaher            #+#    #+#             */
-/*   Updated: 2023/05/12 19:08:05 by azaher           ###   ########.fr       */
+/*   Updated: 2023/05/14 16:25:00 by azaher           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// void	handler(int signal)
-// {
-// 	(void)signal;
-// 	g_exit = 1;
-// 	printf("\n");
-// 	rl_replace_line("", 0);
-// 	rl_on_new_line();
-// 	rl_redisplay();
-// }
+void	handler(int signal)
+{
+	(void)signal;
+	g_exit = 1;
+	printf("\n");
+	rl_replace_line("", 0);
+	rl_on_new_line();
+	rl_redisplay();
+}
 
-// void	handle_signals(void)
-// {
-// 	signal(SIGINT, handler);
-// 	signal(SIGQUIT, SIG_IGN);
-// }
+void	handle_signals(void)
+{
+	signal(SIGINT, handler);
+	signal(SIGQUIT, SIG_IGN);
+}
